1f6e74044cd9a74fb0e2a83f5d7e524d8ca8315d
Joseph Fischetti <fischettijw@gmail.com>
2020-03-16 07:31:48 -0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarStoreApp___cSharp_Console
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+            this.Make = "nothing";
+            this.Model = "nothing";
+            this.Price = 0.00m;
+        }
+
+        public Car(string make, string model, decimal price)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Price = price;
+        }
+    }
+}
